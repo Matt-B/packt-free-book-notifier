6fa64acaebfd33ff6a0e16e2b31c257b38449f6a
Matt Bailey <matt.bailey@velopayments.com>
2019-04-23 17:19:33 +0100

    Rewrite to use puppeteer instead as a simple request no longer returns the title.

--- index.js
+++ index.js
@@ -4,33 +4,24 @@ var async = require('async');
 var PushBullet = require('pushbullet');
 var config = require('./config.json');
 var pusher = new PushBullet(config.pushbullet_api_key);
+var puppeteer = require('puppeteer');
 
 var bookTitle;
 
-async.series([
-
-  function getBookTitle(next) {
-    request("https://www.packtpub.com/packt/offers/free-learning", function(error, response, body) {
-      if(error) {
-        console.log(error);
-      } else {
-        var $ = cheerio.load(body);
-        bookTitle = $('.dotd-title h2').text().trim();
-        next()
-      }
-    });
-  },
-
-  function pushNotification(next) {
-    var deviceParams = {};
-
-    if(bookTitle !== "" && bookTitle) {
-      pusher.note(deviceParams, 'Packt Free Book', 'Today\'s Free Book is ' + bookTitle + '\n'
-        + 'Get it at https://www.packtpub.com/packt/offers/free-learning', next);
-    } else {
-      pusher.note(deviceParams, 'Packt Free Book', 'Something went wrong and I couldn\'t get details of todays book.\n'
-        + 'Please check the website at https://www.packtpub.com/packt/offers/free-learning', next);
-    }
+async function getBookTitle() {
+  var browser = await puppeteer.launch({ headless: true });
+  var page = await browser.newPage();
+  await page.goto('https://www.packtpub.com/packt/offers/free-learning');
+  bookTitle = await page.evaluate(() => document.querySelector('.product__title').textContent);
+  await browser.close();
+  var deviceParams = {};
+  if(bookTitle !== "" && bookTitle) {
+    pusher.note(deviceParams, 'Packt Free Book', 'Today\'s Free Book is ' + bookTitle + '\n'
+      + 'Get it at https://www.packtpub.com/packt/offers/free-learning');
+  } else {
+    pusher.note(deviceParams, 'Packt Free Book', 'Something went wrong and I couldn\'t get details of todays book.\n'
+      + 'Please check the website at https://www.packtpub.com/packt/offers/free-learning');
   }
+};
 
-]);
+getBookTitle();
